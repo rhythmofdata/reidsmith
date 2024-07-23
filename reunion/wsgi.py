b619bc63fb38de960128cd96cc87a518827b0ecc
Hodges <hodgeshaywood@hotmail.com>
2024-07-22 22:38:26 -0400

    settings.py

--- reunion/wsgi.py
+++ reunion/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reunion.settingsprod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reunion.settings")
 
 application = get_wsgi_application()

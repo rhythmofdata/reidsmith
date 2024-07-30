4414828d2101f31f2168ce3399b5d1362ad7d158
Hodges <hodgeshaywood@hotmail.com>
2024-07-30 15:56:02 -0400

    django_tinymce

---
+++ reunion/static/django_tinymce/init_tinymce.js
@@ -0,0 +1,5 @@
+tinymce.init({
+    selector: 'textarea',  // or other selector for your TinyMCE elements
+    plugins: 'lists link image',
+    toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image'
+});
